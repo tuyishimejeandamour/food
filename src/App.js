5ff287127f89315be07761623679ac8dc77f26cf
tuyishimejeandamour <tuyishimejeand88@gmail.com>
2021-07-29 07:51:04 +0200

    finishing expense item

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
     <div className="App">
        <Header />
        <ExpenseItem></ExpenseItem>
+       <ExpenseItem></ExpenseItem>
+       <ExpenseItem></ExpenseItem>
+       <ExpenseItem></ExpenseItem>
     </div>
   );
 }
--- src/components/expense/expense.js
+++ src/components/expense/expense.js
@@ -2,11 +2,22 @@ import './expense.css'
 function ExpenseItem() {
     
     return (
-        <div>
-            <div className="expensse-container">
-               <div className="amount"></div>
+        <div className="hover_div">
+            <div className="expense-container">
+               <div className="amount">
+                   <div className="amount-money">
+                       $20.23
+                   </div>
+                   <div className="divider"></div>
+               </div>
                <div className= "descript">
-
+                <div className="des-container">
+                    <div className="title">title</div>
+                    <div className="descr">description </div>
+                </div>
+                <div className="edit-button">
+                 <span>/</span>
+                </div>
                </div>
             </div>
         </div>
